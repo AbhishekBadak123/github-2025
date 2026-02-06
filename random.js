45520191614b79f321afbd2b60468741466fc594
Anjali <anjaligupta3414@gmail.com>
2026-02-06 17:21:32 +0530

    first

---
+++ random.js
@@ -0,0 +1,63 @@
+import moment from 'moment';
+import random from 'random';
+import jsonfile from 'jsonfile';
+import simpleGit from 'simple-git';
+
+const git = simpleGit();
+const FILE_PATH = './data.json';
+
+// How many active days you want in 2025
+const ACTIVE_DAYS = 400;
+
+const randomTime = (date) => {
+  return moment(date)
+    .hour(random.int(10, 22))
+    .minute(random.int(0, 59))
+    .second(random.int(0, 59));
+};
+
+const run = async () => {
+
+  // Generate all days of 2025
+
+  let days = [];
+
+// range only Dec 6 → Dec 31, 2024
+  let start = moment('2026-01-01');
+  let end   = moment('2025-01-01');
+
+  while (start.isSameOrBefore(end)) {
+    days.push(start.clone());
+    start.add(1, 'day');
+  }
+
+// Shuffle days
+  days = days.sort(() => Math.random() - 0.5);
+
+
+  // Pick random subset
+  const selected = days.slice(0, ACTIVE_DAYS);
+
+  for (let date of selected) {
+
+    const realDate = randomTime(date);
+
+    jsonfile.writeFileSync(FILE_PATH, {
+      date: realDate.format()
+    });
+
+    await git.add([FILE_PATH]);
+
+    await git.commit(`commit ${realDate.format()}`, {
+      "--date": realDate.format()
+    });
+
+    console.log(`✓ ${realDate.format()}`);
+  }
+
+  await git.push('origin', 'main', ['--force']);
+
+  console.log("DONE — RANDOM 2025 FILLED");
+};
+
+run();
